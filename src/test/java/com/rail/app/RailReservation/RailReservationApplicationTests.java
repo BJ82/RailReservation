b7438913d8aff3d7dcadcff999942dffa92bee59
Brahmjyot <brahmjyotsingh@gmail.com>
2025-10-16 01:41:38 +0530

    Created Empty New Project For Rail Reservation

---
+++ src/test/java/com/rail/app/RailReservation/RailReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rail.app.RailReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
