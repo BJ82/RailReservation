f5e1a4dc9d46badc40da7395c3b0f8638d4364ff
Brahmjyot <brahmjyotsingh@gmail.com>
2025-10-19 01:10:01 +0530

    Find All Trains b/w two stations: Updated EnquiryController to use ResponseEntity

--- src/main/java/com/rail/app/RailReservation/Enquiry/Controller/EnquiryController.java
+++ src/main/java/com/rail/app/RailReservation/Enquiry/Controller/EnquiryController.java
@@ -4,11 +4,14 @@ import com.rail.app.RailReservation.Enquiry.Service.Enquiry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.rail.app.RailReservation.Enquiry.DTO.TrainEnquiryResponse;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,10 +26,16 @@ public class EnquiryController {
     private Enquiry enquiryService;
 
     @GetMapping("train")
-    public List<TrainEnquiryResponse> trainEnquiryByStation(@RequestParam String src,@RequestParam String dest){
+    public ResponseEntity<List<TrainEnquiryResponse>> trainEnquiryByStation(@RequestParam String src,@RequestParam String dest){
 
         logger.info(COMMON_MESSAGE);
         logger.info("Processing request to find all trains between {} and {}",src,dest);
-        return enquiryService.trainEnquiry(src,dest);
+
+        List<TrainEnquiryResponse> trainsFound = enquiryService.trainEnquiry(src,dest);
+        if(trainsFound.isEmpty()){
+            return ResponseEntity.badRequest().body(trainsFound);
+        }
+        return ResponseEntity.ok().body(trainsFound);
     }
+
 }
