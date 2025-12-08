38f35dfbc416da8fd29c4e94e209b3a47afd8b96
Brahmjyot <brahmjyotsingh@gmail.com>
2025-12-09 04:35:17 +0530

    Added Profiles

--- src/main/java/com/rail/app/railreservation/RailReservationApplication.java
+++ src/main/java/com/rail/app/railreservation/RailReservationApplication.java
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class RailReservationApplication {
@@ -13,7 +12,7 @@ public class RailReservationApplication {
 
 	public static void main(String[] args) {
 
-		logger.info("Starting Rail Reservation System....");
+		logger.info("Starting Passenger Enquiry & Reservation System (PERS)....");
 		SpringApplication.run(RailReservationApplication.class, args);
 	}
 
